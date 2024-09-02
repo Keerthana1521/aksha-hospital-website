8c4b10108195041577325b44c07a5a7672b91077
keerthana <keerthana.m@iamneo.ai>
2024-09-02 23:23:54 +0530

    contact-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
