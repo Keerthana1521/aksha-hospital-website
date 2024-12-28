0fadb1d3aab87bb08dbefb3fc375abbbf169d85c
Rithish06 <rithish.manohar006@gmail.com>
2024-12-28 12:21:30 +0530

    Image optimization home page SEO

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+constructor(private titleService: Title, private metaService: Meta) {
+    this.titleService.setTitle("About Aksha Hospital | Leading Healthcare in Jayanagar");  
 
+    this.metaService.updateTag({ name: 'description', content: "Learn about Aksha Hospital's commitment to providing excellent healthcare in Jayanagar with a patient-centric approach." });
+  
+    this.metaService.updateTag({ name: 'keywords', content: 'Aksha Hospital about us, healthcare excellence Jayanagar, patient care Bangalore, hospital mission' }
+
+    );
+  }
 }
+
+// export class AboutComponent {
+//   constructor(private titleService: Title, private metaService: Meta) {
+//       this.titleService.setTitle("");  
+  
+//       this.metaService.updateTag({ name: 'description', content: '' });
+    
+//       this.metaService.updateTag({ name: 'keywords', content: '' }
+        
+//       );
+//     }
+//   }
